26082cbfbbcbc4141ddde47f86635b2d0b338e72
Lightningmash.Dennis <Lightningmash.machariadenniskamau@gmail.com>
2025-07-29 13:15:38 +0300

    Modified a copy alx-react-app

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <>
+            <div style={{ backgroundColor: '#f0f8ff', padding: '10px', textAlign: 'center' }}>
+                <footer>
+                    <p style={{ fontSize: '16px', color: '#555' }}>
+                        © 2023 City Lovers
+                    </p>
+                </footer>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
